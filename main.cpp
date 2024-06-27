4fbf50ed8bd1c42027a67054ed3e389b16dc71ff
ShreyaMathur19 <145053811+ShreyaMathur19@users.noreply.github.com>
2024-06-27 17:36:19 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * Binary Search Tree Visualization
+ * Ashland University
+ * Nathan Ahrens
+ * Summer 2017
+ *
+ * Begins the program.
+ */
+
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
